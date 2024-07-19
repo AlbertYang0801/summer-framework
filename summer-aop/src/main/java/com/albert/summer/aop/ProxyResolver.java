4be8b661557f682cb7b695460a65be4bbb8af16e
Albert.Yang <18438049166@163.com>
2024-07-19 17:58:54 +0800

    [feat]init summer-aop moudle

---
+++ summer-aop/src/main/java/com/albert/summer/aop/ProxyResolver.java
@@ -0,0 +1,13 @@
+package com.albert.summer.aop;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author yangjunwei
+ * @date 2024/7/19
+ */
+@Slf4j
+public class ProxyResolver {
+
+
+}
---
+++ summer-aop/src/main/java/com/albert/summer/exception/AopConfigException.java
@@ -0,0 +1,20 @@
+package com.albert.summer.exception;
+
+public class AopConfigException extends NestedRuntimeException {
+
+    public AopConfigException() {
+        super();
+    }
+
+    public AopConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AopConfigException(String message) {
+        super(message);
+    }
+
+    public AopConfigException(Throwable cause) {
+        super(cause);
+    }
+}
